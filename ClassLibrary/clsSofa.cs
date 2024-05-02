a0a64d4a521863834b9b95dc48b8c107cca15015
P2713128 <P2713128@my365.dmu.ac.uk>
2024-05-02 09:29:00 +0100

    Added testing for the sofa class

---
+++ ClassLibrary/clsSofa.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class clsSofa
+    {
+        public clsSofa()
+        {
+        }
+    }
+}
---
+++ Testing3/tstSofa.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstSofa
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsSofa AnAddress = new clsSofa();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
