ee4ccd0702fd5bc267c2e2382d2741431ae75d6b
P2713128 <P2713128@my365.dmu.ac.uk>
2024-05-07 14:14:27 +0100

    Added test data to sofa class

---
+++ dbo.tblSofa.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblSofa] (
+    [SofaId]          INT          NOT NULL,
+    [SofaDescription] TEXT         NULL,
+    [SofaColour]      TEXT         NOT NULL,
+    [SupplierId]      INT          NOT NULL,
+    [Price]           DECIMAL (18) NOT NULL,
+    [Available]       BIT          NOT NULL,
+    [DateAdded]       DATE         NULL,
+    PRIMARY KEY CLUSTERED ([SofaId] ASC)
+);
+
