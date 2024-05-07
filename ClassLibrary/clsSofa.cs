b75bcd04e5adb79d74092799891209270d938e13
P2713128 <P2713128@my365.dmu.ac.uk>
2024-05-07 13:57:10 +0100

    Added test data for the sofa class

--- ClassLibrary/clsSofa.cs
+++ ClassLibrary/clsSofa.cs
@@ -1,6 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSofa
     {
+        public bool Active { get; set; }
+        public int SofaId { get; set; }
+        public int SupplierId { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string SofaDescription { get; set; }
+        public string Colour { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
     }
 }
